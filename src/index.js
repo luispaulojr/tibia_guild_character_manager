f33a1d49b7c2d6b3444f587c63c1fff97a3fc6b5
Luis Lessa <prof.lpjunior@gmail.com>
2024-07-28 18:04:46 -0300

    WIP: Correção na emissão dos dados

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 
 const app = express();
 const server = http.createServer(app);
+const apiUrl = 'https://api.tibiadata.com/v4';
 const io = socketIo(server, {
     cors: {
         origin: "*", // Allow requests from the React app
@@ -12,12 +13,33 @@ const io = socketIo(server, {
     }
 });
 
+const fetchCharacterData = async (characterName) => {
+    try {
+        const response = await axios.get(`${apiUrl}/character/${characterName}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching character data for ${characterName}:`, error);
+        throw error;
+    }
+};
+
+const fetchGuildData = async (guildName) => {
+    try {
+        const response = await axios.get(`${apiUrl}/guild/${guildName}`);
+        console.log(`Guild data for ${guildName}:`, response.data); // Log the response data
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching guild data for ${guildName}:`, error);
+        throw error;
+    }
+};
+
 app.get('/api/character/:name', async (req, res) => {
     const characterName = req.params.name;
     try {
         console.log(`Fetching data for ${characterName}`);
-        const response = await axios.get(`https://api.tibiadata.com/v4/character/${characterName}`);
-        res.json(response.data);
+        const data = await fetchCharacterData(characterName);
+        res.json(data);
     } catch (error) {
         res.status(500).send('Error fetching character data');
     }
@@ -27,8 +49,8 @@ app.get('/api/guild/:name', async (req, res) => {
     const guildName = req.params.name;
     try {
         console.log(`Fetching data for ${guildName}`);
-        const response = await axios.get(`https://api.tibiadata.com/v4/guild/${guildName}`);
-        res.json(response.data);
+        const data = await fetchGuildData(guildName);
+        res.json(data);
     } catch (error) {
         res.status(500).send('Error fetching guild data');
     }
@@ -37,10 +59,33 @@ app.get('/api/guild/:name', async (req, res) => {
 io.on('connection', (socket) => {
     console.log('New client connected');
 
-    // Simulate character status updates for testing
-    setInterval(() => {
-        socket.emit('statusUpdate', { name: 'CharacterName', status: 'online' });
-    }, 5000);
+    // Function to check and emit status of guild members
+    const checkGuildMembersStatus = async (guildName) => {
+        try {
+            const guildData = await fetchGuildData(guildName);
+            if (guildData.guild && guildData.guild.members) {
+                const members = guildData.guild.members;
+
+                for (const member of members) {
+                    const characterData = await fetchCharacterData(member.name);
+                    if (characterData.character) {
+                        const character = characterData.character.character;
+                        socket.emit('statusUpdate', { name: member.name, status: member.status, level:  character.level, vocation: character.vocation, guild_rank: character.guild.rank });
+                    } else {
+                        console.error(`Character data for ${member.name} is missing 'data' property`);
+                    }
+                }
+            } else {
+                console.error(`Guild data for ${guildName} is missing 'guild' or 'members' property`);
+            }
+        } catch (error) {
+            console.error('Error fetching guild members status:', error);
+        }
+    };
+
+    // Check status of guild members every 60 seconds
+    const guildName = "Gangue do Meubom"; // Replace with the actual guild name
+    setInterval(() => checkGuildMembersStatus(guildName), 10000);
 
     socket.on('disconnect', () => {
         console.log('Client disconnected');
